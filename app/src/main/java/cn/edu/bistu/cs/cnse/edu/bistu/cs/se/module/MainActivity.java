224c4c5a227b54095483e0cb7db6d223ec1f1516
1447lhx <1959142791@qq.com>
2016-09-05 19:53:31 +0800

    update exercise_3

--- app/src/main/java/cn/edu/bistu/cs/cnse/edu/bistu/cs/se/module/MainActivity.java
+++ app/src/main/java/cn/edu/bistu/cs/cnse/edu/bistu/cs/se/module/MainActivity.java
@@ -1,56 +1,69 @@
 package cn.edu.bistu.cs.cnse.edu.bistu.cs.se.module;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+ import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+ import java.util.HashMap;
+ import java.util.List;
+ import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn=(Button)findViewById(R.id.Button);
-        final TextView textView=(TextView)findViewById(R.id.textView);
-        btn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.content_main);
+        Button button=(Button)findViewById(R.id.btn_enter);
+        final TextView txtResult=(TextView)findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
-                textView.setText(textView.getText());
+                txtResult.setText(Integer.parseInt(txtResult.getText() + "") + 1 + "");
             }
         });
+        }
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+        TextView textView=(TextView)findViewById(R.id.textView);
+        switch(view.getId()) {
+            case R.id.checkBox1:
+                if(checked){
+                    textView.setText("123");
+                }
+                break;
+            case R.id.checkBox2:
+                if(checked){
+                    textView.setText("456");
+                }
+
+
+        }
     }
     public void onRadioButtonClicked(View view) {
+
         boolean checked = ((RadioButton) view).isChecked();
         TextView textView=(TextView)findViewById(R.id.textView);
         switch(view.getId()) {
             case R.id.radioButton1:
                 if (checked)
-                    textView.setText("3874922");
+                    textView.setText("1234");
                 break;
             case R.id.radioButton2:
                 if (checked)
-                    textView.setText("387489");
+                    textView.setText("5678");
                 break;
         }
     }
-    public void onCheckBoxClicked(View view) {
 
-        boolean checked = ((CheckBox) view).isChecked();
-        TextView textView = (TextView) findViewById(R.id.textView);
-        switch (view.getId()) {
-            case R.id.checkBox1:
-                if (checked)
-                    textView.setText("1234567");
-                break;
-            case R.id.checkBox2:
-                if (checked)
-                    textView.setText("7384921");
-                break;
-        }
-    }}
-
-
+}
